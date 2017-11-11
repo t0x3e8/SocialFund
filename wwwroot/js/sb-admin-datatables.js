472302ae83408168e8562936fe99a1abd05444a3
Jarek <t0x3e8@gmail.com>
2017-11-11 10:38:23 +0100

    language and styling fix

--- wwwroot/js/sb-admin-datatables.js
+++ wwwroot/js/sb-admin-datatables.js
@@ -1,4 +1,19 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+  $('#dataTable').DataTable({
+    "language" : {
+        "lengthMenu": "Wyświetl _MENU_ rekordów na stronie",
+        "zeroRecords": "Brak rekordów",
+        "info": "Strona _PAGE_ z _PAGES_",
+        "infoEmpty": "Brak rekordów",
+        "infoFiltered": "(filtered from _MAX_ total records)",
+        "search" : "Wyszukaj:",
+        paginate: {
+          first:      "Pierwszy",
+          previous:   "Poprzedni",
+          next:       "Następny",
+          last:       "Ostatni"
+      },
+    }
+  });
 });
