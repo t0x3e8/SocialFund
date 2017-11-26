d907e4135a34b18c0ad8ddc1fa4af166c107881e
Jarek <t0x3e8@gmail.com>
2017-11-26 21:02:05 +0100

    Again

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 
+// copy of data.json file in order to satify Unit Tests
 gulp.task('prepUnitTests', function() {
     gulp.src('SF.App/data.json')
         .pipe(gulp.dest('SF.Tests/bin/Debug/netcoreapp2.0/'));
