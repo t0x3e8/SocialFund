e6aa87dff8fdc0a820f4e94690e1e9f79d1a9f0a
Jarek <t0x3e8@gmail.com>
2017-11-29 20:48:55 +0100

    update of vendors js and css

--- deploy.cmd
+++
@@ -1,6 +0,0 @@
-if [ -e "$DEPLOYMENT_TARGET/SF.App/bower.json" ]; then
-  cd "$DEPLOYMENT_TARGET/SF.App/"
-  eval ./node_modules/.bin/bower install
-  exitWithMessageOnError "bower failed"
-  cd - > /dev/null
-fi
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,42 @@
 var gulp = require('gulp');
 
 // copy of data.json file in order to satify Unit Tests
-gulp.task('prepUnitTests', function() {
+gulp.task('prepUnitTests', function () {
     gulp.src('SF.App/data.json')
         .pipe(gulp.dest('SF.Tests/bin/Debug/netcoreapp2.0/'));
+})
+
+gulp.task('copyLibs', function () {
+    // font awesome
+    gulp.src('SF.App/bower/components-font-awesome/css/*.css')
+        .pipe(gulp.dest('SF.App/wwwroot/css/lib/'));
+
+    gulp.src('SF.App/bower/components-font-awesome/fonts/*')
+        .pipe(gulp.dest('SF.App/wwwroot/css/fonts/'));
+
+    // bootstrap
+    // gulp.src('SF.App/bower/bootstrap/dist/css/bootstrap.*css')
+    //     .pipe(gulp.dest('SF.App/wwwroot/css/lib/'));
+
+    // gulp.src('SF.App/bower/bootstrap/dist/js/bootstrap.*js')
+    //     .pipe(gulp.dest('SF.App/wwwroot/js/lib/'));
+
+    // datatables
+    gulp.src('SF.App/bower/datatables.net-bs4/css/dataTables.bootstrap4.*css')
+        .pipe(gulp.dest('SF.App/wwwroot/css/lib/'));
+
+    gulp.src('SF.App/bower/datatables.net-bs4/js/dataTables.bootstrap4.*js')
+        .pipe(gulp.dest('SF.App/wwwroot/js/lib/'));
+
+    gulp.src('SF.App/bower/datatables.net/js/jquery.dataTables.*js')
+        .pipe(gulp.dest('SF.App/wwwroot/js/lib/'));
+
+    // jquery
+    gulp.src('SF.App/bower/jquery/dist/jquery.*js')
+        .pipe(gulp.dest('SF.App/wwwroot/js/lib/'));
+
+    // jquery easing
+    gulp.src('SF.App/bower/jquery-easing/jquery.easing.*js')
+        .pipe(gulp.dest('SF.App/wwwroot/js/lib/'));
+
 })
