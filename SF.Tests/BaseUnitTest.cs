c384e6f0cc563164b8c5165c2a1eea799128f42a
t0x3e8 <t0x3e8@gmail.com>
2018-02-18 14:55:07 +0100

    fix

--- SF.Tests/BaseUnitTest.cs
+++ SF.Tests/BaseUnitTest.cs
@@ -4,9 +4,11 @@ using AutoMapper;
 public class BaseUnitTest : IDisposable
 {
     public BaseUnitTest() {
-        var mappingProfile = new SF.App.Models.MappingProfile();
-        Mapper.Initialize(m => m.AddProfile(mappingProfile));
-        Mapper.AssertConfigurationIsValid();
+        if (Mapper.Instance == null) {
+            var mappingProfile = new SF.App.Models.MappingProfile();
+            Mapper.Initialize(m => m.AddProfile(mappingProfile));
+            Mapper.AssertConfigurationIsValid();
+        }
     }
     public void Dispose() {
         Mapper.Reset();
